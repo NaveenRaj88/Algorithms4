a088122d1d0c67abcc2ef516414e36df4af3a20f
Naveen <rajnaveen0488@gmail.com>
2016-06-28 22:50:21 +0530

    More algo

--- src/com/chap01/sec1_1/Ex_1_1_15_histogram.java
+++ src/com/chap01/sec1_1/Ex_1_1_15_histogram.java
@@ -1,15 +1,18 @@
 package com.chap01.sec1_1;
 
+import com.util.ArrayGenerator;
+
 /**
  * Created by Naveen Kumar.A on 27/06/16.
  */
 public class Ex_1_1_15_histogram {
 
     public static int[] histogram(int[] a){
-
+    return a;
     }
 
     public static void main(String[] args) {
+        int[]  a = ArrayGenerator.generateAscendingIntArray(10);
 
     }
 }
