68fe9b1a5d4836499bd504ac8ffd9468fd54c94f
Naveen Kumar <rajnaveen0488@gmail.com>
2016-07-17 18:39:22 +0530

    More Algo

--- src/com/chap01/sec1_1/Ex_1_1_11.java
+++ src/com/chap01/sec1_1/Ex_1_1_11.java
@@ -2,19 +2,19 @@ package com.chap01.sec1_1;
 
 /**
  * Write a code fragment that prints the contents of a two-dimensional boolean
- * array, using '*' to represent true and a space to represent false. Include row and column
- * numbers.
- * Created by Naveen Kumar.A on 27/06/16.
+ * array, using '*' to represent true and a space to represent false. Include
+ * row and column numbers. Created by Naveen Kumar.A on 27/06/16.
  */
 public class Ex_1_1_11 {
 
-    public static void main(String[] args) {
-        boolean[][] boo = new boolean[][]{{true, false, true, false}, {false, true, true, true}, {true, false, true, true}, {true, false, false, true}};
-        for (int i = 0; i < boo.length; i++) {
-            System.out.println();
-            for (int j = 0; j < boo.length; j++) {
-                System.out.print(boo[i][j] + "  ");
-            }
-        }
-    }
+	public static void main(String[] args) {
+		boolean[][] boo = new boolean[][] { { true, false, true, false }, { false, true, true, true },
+				{ true, false, true, true }, { true, false, false, true } };
+		for (int i = 0; i < boo.length; i++) {
+			System.out.println();
+			for (int j = 0; j < boo.length; j++) {
+				System.out.print(boo[i][j] + "  ");
+			}
+		}
+	}
 }
