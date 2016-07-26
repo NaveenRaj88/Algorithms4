f7317f9d1b14ed84269facf1c203f9be07ba4033
Naveen <rajnaveen0488@gmail.com>
2016-07-26 23:55:51 +0530

    More algo

--- src/com/chap01/sec1_5/QuickUnion.java
+++ src/com/chap01/sec1_5/QuickUnion.java
@@ -4,42 +4,41 @@ import com.util.In;
 
 public class QuickUnion extends UF {
 
-	public QuickUnion(int n) {
-		super(n);
-	}
+    public QuickUnion(int n) {
+        super(n);
+    }
 
-	@Override
-	public void union(int p, int q) {
-		int pRoot = find(p);
-		int qRoot = find(q);
-		if(pRoot == qRoot){
-			return;
-		}
-		id[pRoot] = qRoot;
-		count--;
-	}
+    @Override
+    public void union(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+        if (pRoot != qRoot) {
+            id[pRoot] = qRoot;
+            count--;
+        }
+    }
 
-	@Override
-	public int find(int p) {
-		while(p != id[p]){
-			p=id[p];
-		}
-		return p;
-	}
+    @Override
+    public int find(int p) {
+        while (p != id[p]) {
+            p = id[p];
+        }
+        return p;
+    }
 
-	public static void main(String[] args) {
-		int[] a = In.readInts("resources/tinyUf.txt");
-		int index = 0;
-		QuickUnion qu = new QuickUnion(a[index++]);
-		while (index < a.length) {
-			int p = a[index++];
-			int q = a[index++];
-			if (qu.connected(p, q)) {
-				continue;
-			}
-			qu.union(p, q);
-			System.out.println(p + " " + q);
-		}
-		System.out.println(qu.count + " components");
-	}
+    public static void main(String[] args) {
+        int[] a = In.readInts("resources/tinyUf.txt");
+        int index = 0;
+        QuickUnion qu = new QuickUnion(a[index++]);
+        while (index < a.length) {
+            int p = a[index++];
+            int q = a[index++];
+            if (qu.connected(p, q)) {
+                continue;
+            }
+            qu.union(p, q);
+            System.out.println(p + " " + q);
+        }
+        System.out.println(qu.count + " components");
+    }
 }
