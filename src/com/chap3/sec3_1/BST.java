76da5c0b0fc9a5f2bd879bad336aa547e2cfc552
Naveen Kumar <rajnaveen0488@gmail.com>
2016-10-17 16:42:38 +0530

    More Algo

--- src/com/chap3/sec3_1/BST.java
+++ src/com/chap3/sec3_1/BST.java
@@ -10,58 +10,139 @@ public class BST<Key extends Comparable<Key>, Value> {
 
     private Node<Key, Value> root;
 
-    public int size(Node root){
-        if(root == null){
+    public int size(Node root) {
+        if (root == null) {
             return 0;
         }
         return root.n;
     }
 
-    public int calcSize(Node root){
-        if(root == null){
+    public int calcSize(Node root) {
+        if (root == null) {
             return 0;
         }
-        return size(root.left)+size(root.right)+1;
+        return size(root.left) + size(root.right) + 1;
     }
 
-    public Value get(Key key){
+    public Value get(Key key) {
         root.key.compareTo(key);
         key.compareTo(root.key);
-       return get(root, key);
+        return get(root, key);
     }
 
-    private Value get(Node<Key,Value> x, Key key){
-        if(x == null){
+    private Value get(Node<Key, Value> x, Key key) {
+        if (x == null) {
             return null;
         }
         int cmp = key.compareTo(x.key);
-        if(cmp < 0){
+        if (cmp < 0) {
             get(x.left, key);
-        }else if(cmp > 0){
+        } else if (cmp > 0) {
             get(x.right, key);
-        }else{
+        } else {
             return x.value;
         }
         return null;
     }
 
-    public void put(Key key, Value value){
+    public void put(Key key, Value value) {
 
     }
 
-    private Node put(Node<Key, Value> root, Key key, Value value){
-        if(key == null){
+    private Node put(Node<Key, Value> root, Key key, Value value) {
+        if (key == null) {
             return new Node(key, value, 1);
         }
         int cmp = key.compareTo(root.key);
-        if(cmp < 0 ){
+        if (cmp < 0) {
             root.left = put(root.left, key, value);
-        }else if(cmp > 0){
+        } else if (cmp > 0) {
             root.right = put(root.right, key, value);
-        }else{
+        } else {
             root.value = value;
         }
+        root.n = size(root.left) + size(root.right) + 1;
+        return root;
+    }
+
+    public Key min() {
+        return min(root).key;
+    }
+
+    private Node<Key,Value> min(Node root) {
+        if (root == null) {
+            return null;
+        }
+        if (root.left == null) {
+            return root;
+        }
+        return min(root.left);
+    }
+
+    public Key floor(Key key){
+        return root.key;
+    }
+
+    private Node<Key,Value> floor(Node<Key, Value> root, Key key){
+        if(root == null){
+            return null;
+        }
+        int cmp =  key.compareTo(root.key);
+        if(cmp == 0){
+            return root;
+        }
+        if(cmp<0){
+            return floor(root.left, key);
+        }
+        Node t = floor(root.right,key);
+        if(t != null){
+            return t;
+        }else{
+            return root;
+        }
+    }
+
+    public void deleteMin(){
+        root = deleteMin(root);
+    }
+
+    private Node deleteMin(Node<Key, Value> root){
+        if(root == null){
+            return null;
+        }
+        if(root.left == null){
+           return root.right;
+        }
+        root.left = deleteMin(root.left);
         root.n = size(root.left)+ size(root.right)+1;
+        return root;
+    }
+
+    public void delete(Key key){
+        root = delete(root, key);
+    }
+
+    private Node delete(Node<Key, Value> root, Key key){
+        if(root == null){
+            return null;
+        }
+        int cmp = key.compareTo(root.key);
+        if(cmp < 0){
+            root.left = delete(root.left,key);
+        }else if(cmp > 0){
+            root.right = delete(root.right,key);
+        }else{
+            if(root.right == null){
+                return root.left;
+            }if(root.left == null){
+                return root.right;
+            }
+            Node t = root;
+            root = min(t.right);
+            root.right = deleteMin(t.right);
+            root.left = t.left;
+        }
+        root.n = size(root.left)+size(root.right)+1;
         return root;
     }
 }
--- src/com/chap3/sec3_3/RedBlackBST.java
+++ src/com/chap3/sec3_3/RedBlackBST.java
@@ -25,6 +25,8 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
         }
     }
 
+    private Node root;
+
     private boolean isRed(Node h) {
         if (h == null) {
             return BLACK;
@@ -32,7 +34,64 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
         return h.color == RED;
     }
 
+    private Node rotateLeft(Node h){
+        Node x = h.right;
+        h.right = x.left;
+        x.left = h;
+        x.color = h.color;
+        x.color = RED;
+        x.n = h.n;
+        h.n = 1+size(h.left)+size(h.right);
+        return x;
+    }
 
+    private Node rotateRight(Node h){
+        Node x = h.left;
+        h.left = x.right;
+        x.right = h;
+        x.color = h.color;
+        h.color=RED;
+        x.n = h.n;
+        h.n = 1+size(h.left)+size(h.right);
+        return x;
+    }
+
+    private void flipColors(Node h){
+        h.color = RED;
+        h.right.color = BLACK;
+        h.left.color = BLACK;
+    }
+
+    private int size(){
+        return size(root);
+    }
+
+    private int size(Node x){
+        if(x == null){
+            return 0;
+        }
+        return 1+size(x.left)+size(x.right);
+    }
+
+    public void put(Key key, Value val)
+    { // Search for key. Update value if found; grow table if new.
+        root = put(root, key, val);
+        root.color = BLACK;
+    }
+    private Node put(Node h, Key key, Value val)
+    {
+        if (h == null) // Do standard insert, with red link to parent.
+            return new Node(key, val, 1, RED);
+        int cmp = key.compareTo(h.key);
+        if (cmp < 0) h.left = put(h.left, key, val);
+        else if (cmp > 0) h.right = put(h.right, key, val);
+        else h.value = val;
+        if (isRed(h.right) && !isRed(h.left)) h = rotateLeft(h);
+        if (isRed(h.left) && isRed(h.left.left)) h = rotateRight(h);
+        if (isRed(h.left) && isRed(h.right)) flipColors(h);
+        h.n = size(h.left) + size(h.right) + 1;
+        return h;
+    }
 
     public static void main(String[] args) {
 
