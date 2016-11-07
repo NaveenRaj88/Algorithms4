ade73c9d62132ee0b5542f03cf2298910631d4a3
Naveen Kumar <rajnaveen0488@gmail.com>
2016-11-07 13:00:50 +0530

    More Algo

--- src/com/chap05/sec5_3/BruteForceAlternative.java
+++ src/com/chap05/sec5_3/BruteForceAlternative.java
@@ -5,5 +5,23 @@ package com.chap05.sec5_3;
  */
 public class BruteForceAlternative {
 
+    public static int search(String text, String pat) {
+        int j, m = pat.length();
+        int i, n = text.length();
+        for (i = 0, j = 0; i < n && j < m; i++) {
+            if (text.charAt(i) == pat.charAt(j)) {
+                j++;
+            } else {
+                i -= j;
+                j = 0;
+            }
+
+        }
+        if (j == m) {
+            return i - m;
+        } else {
+            return n;
+        }
+    }
 
 }
---
+++ src/com/chap05/sec5_3/RabinKarp.java
@@ -0,0 +1,9 @@
+package com.chap05.sec5_3;
+
+/**
+ * Created by Naveen Kumar .A on 11/7/16.
+ */
+public class RabinKarp {
+
+
+}
